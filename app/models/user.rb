1cee3c446a48a1d0fa70eb527cddf7a036a017b8
bogunov <bogunov@gmail.com>
2012-05-19 16:59:19 +0400

    added unique email check

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :user_type
-  validates :email,  :presence => true, :length => { :minimum => 6 }
+  validates :email,  :presence => true, :length => { :minimum => 6 }, :uniqueness => true
 end
