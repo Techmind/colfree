ef2b8122650372f8e3d346ab1ea9ea94f3ff4b43
bogunov <bogunov@gmail.com>
2012-05-19 12:18:08 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vlab::Application.config.secret_token = 'c3f6d0c71c94804781b6cb1b0582f8162824520dadb5e513fa9c0a12ae14d720009908d1a1e986bdb543c91c088e4d164e9417ea7b7f11b52e4cb9b8cd72c8cd'
